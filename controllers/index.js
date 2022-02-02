59459741816e603a59cab7e2a1b81060ef84f50d
Kevin Lacson <lacsonky@gmail.com>
2022-02-02 18:38:38 -0500

    styling

--- controllers/index.js
+++ controllers/index.js
@@ -5,5 +5,8 @@ const apiRoutes = require("./api");
 router.get("/", (req, res) => {
     res.render("home");
 });
+router.get("/login", (req, res) => {
+    res.render("login");
+});
 
 module.exports = router;
