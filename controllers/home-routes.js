9358d8319d4be3a557a997eb6dbdf72578a8135a
Kevin Lacson <lacsonky@gmail.com>
2022-02-08 12:30:06 -0500

    success: rendered posts to dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,11 +17,21 @@ router.get("/", (req, res) => {
 
 // desc: dashboard view
 // GET /dashboard
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", async (req, res) => {
     if (req.session.loggedIn) {
+        const posts = await Post.findAll({
+            where: {
+                user_id: req.session.userData.id,
+            },
+            attributes: {
+                exclude: ["id", "user_id"],
+            },
+        });
+        const postMap = posts.map(post => post.get({ plain: true }));
         res.render("dashboard", {
             loggedIn: req.session.loggedIn,
             userData: req.session.userData,
+            postMap,
         });
         return;
     } else {
